db66064c6f3e22f9b9e6a0dbb0ae82e0fc0a23ba
kaoru <k@warpnine.io>
2024-01-05 18:58:17 +0900

    fix: update models

--- src/model.rs
+++ src/model.rs
@@ -12,13 +12,9 @@ impl FromStr for Model {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "gpt-4" => Ok(Self::Gpt4),
-            "4" => Ok(Self::Gpt4), // alias
-            "gpt-4-32k" => Ok(Self::Gpt4_32k),
-            "32k" => Ok(Self::Gpt4_32k), // alias
-            "gpt-3.5-turbo" => Ok(Self::Gpt35Turbo),
-            "35t" => Ok(Self::Gpt35Turbo), // alias
-            "35" => Ok(Self::Gpt35Turbo),  // alias
+            "gpt-4" | "4" => Ok(Self::Gpt4),
+            "gpt-4-32k" | "32k" => Ok(Self::Gpt4_32k),
+            "gpt-3.5-turbo" | "35t" | "35" => Ok(Self::Gpt35Turbo),
             _ => Ok(Self::Gpt35Turbo),
         }
     }
@@ -27,9 +23,9 @@ impl FromStr for Model {
 impl Display for Model {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let s = match self {
-            Self::Gpt4 => "gpt-4",
+            Self::Gpt4 => "gpt-4-1106-preview",
             Self::Gpt4_32k => "gpt-4-32k",
-            Self::Gpt35Turbo => "gpt-3.5-turbo",
+            Self::Gpt35Turbo => "gpt-3.5-turbo-1106",
         };
         write!(f, "{}", s)
     }
